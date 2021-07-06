bc764796771025095a9deb26f76054b1c995d178
WaterKat_ROG <WaterKatGames@gmail.com>
2021-07-06 02:51:53 -0700

    Added Timer Class for object based time keeping

---
+++ Runtime/Timer.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaterKat.CustomTime
+{
+    [Serializable]
+    public class Timer
+    {
+        private float currentTick = 0;
+        private float goalTick = 1;
+
+        public float currentTime { get { return currentTick; } }
+        public float timeLeft { get { return goalTick - currentTick; } }
+        public float goalTime { get { return goalTick; } }
+
+        public bool Tick(float deltaTime)
+        {
+            currentTick += deltaTime;
+            return currentTick >= goalTick;
+        }
+
+        public void RestartTimer()
+        {
+            currentTick = 0;
+        }
+
+        public Timer(float desiredTime)
+        {
+            goalTick = desiredTime;
+        }
+    }
+}
